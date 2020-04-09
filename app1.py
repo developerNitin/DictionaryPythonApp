6d59d7d4b21afe2d55bd3bcbc00ca9713ad2b307
Nitin Birdi <nitin.birdi@icloud.com>
2020-04-10 00:26:43 +0530

    #8 commit

--- app1.py
+++ app1.py
@@ -1,3 +1,23 @@
-print("hello");
-x = 10;
-print("hello");
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+i = 1
+
+def Word():
+   word = input("enter a word: ").lower()
+   return word
+
+def translate(word):
+  if word in data:
+     print(data[word])
+     return 0
+  elif get_close_matches(word, data.keys()) != []:                    #alt len(get_close_matches(word, data.keys()))
+    print ("do you mean: %s" % (get_close_matches(word, data.keys())))
+    return Word()
+  else:
+    print("word doesn't exist, please double check") 
+    return Word()
+
+translate(Word())
