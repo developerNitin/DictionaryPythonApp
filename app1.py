f81d66f21a103686c681f653d9b2c24073969bc7
Nitin Birdi <nitin.birdi@icloud.com>
2020-04-10 00:33:20 +0530

    #9 commit

--- app1.py
+++ app1.py
@@ -9,15 +9,20 @@ def Word():
    word = input("enter a word: ").lower()
    return word
 
+def Word2():
+   word2 = input("enter a word: ").lower()
+   return translate(word2)
+
 def translate(word):
   if word in data:
      print(data[word])
      return 0
-  elif get_close_matches(word, data.keys()) != []:                    #alt len(get_close_matches(word, data.keys()))
+  elif get_close_matches(word, data.keys()) != []: 
+  #alt len(get_close_matches(word, data.keys()))                   
     print ("do you mean: %s" % (get_close_matches(word, data.keys())))
-    return Word()
+    return Word2()
   else:
     print("word doesn't exist, please double check") 
-    return Word()
+    return Word2()
 
 translate(Word())
